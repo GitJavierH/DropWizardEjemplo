e1abe9122706330e381d9d7d3a3d748e23c87e35
Javier Hernandez <javierr.hernandez@gmail.com>
2023-09-11 09:44:53 -0500

    Actualización del 2023-09-11

--- src/main/java/com/java/demo/App.java
+++ src/main/java/com/java/demo/App.java
@@ -2,25 +2,62 @@ package com.java.demo;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
+import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
+import javax.ws.rs.client.Client;
+
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.java.demo.basicauth.AppAuthenticator;
+import com.java.demo.basicauth.AppAuthorizer;
+import com.java.demo.basicauth.User;
+import com.java.demo.controller.EmployeeRESTController;
+import com.java.demo.controller.RESTClientController;
+import com.java.demo.healthcheck.AppHealthCheck;
+import com.java.demo.healthcheck.HealthCheckController;
+
 public class App extends Application<Configuration> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    @Override
-    public void initialize(Bootstrap<Configuration> b) {
-    }
+	@Override
+	public void initialize(Bootstrap<Configuration> b) {
+	}
 
-    @Override
-    public void run(Configuration c, Environment e) throws Exception {
-        LOGGER.info("Registrando recursos REST");
-        //e.jersey().register(new EmployeeRESTController(e.getValidator()));
-    }
+	@Override
+	public void run(Configuration c, Environment e) throws Exception 
+	{
+		LOGGER.info("Registering REST resources");
+		
+		e.jersey().register(new EmployeeRESTController(e.getValidator()));
 
-    public static void main(String[] args) throws Exception {
-        new App().run(args);
-    }
+		final Client client = new JerseyClientBuilder(e)
+				.build("DemoRESTClient");
+		e.jersey().register(new RESTClientController(client));
+
+		// Application health check
+		e.healthChecks().register("APIHealthCheck", new AppHealthCheck(client));
+
+		// Run multiple health checks
+		e.jersey().register(new HealthCheckController(e.healthChecks()));
+		
+		//Setup Basic Security
+		e.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<User>()
+                .setAuthenticator(new AppAuthenticator())
+                .setAuthorizer(new AppAuthorizer())
+                .setRealm("App Security")
+                .buildAuthFilter()));
+        e.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+        e.jersey().register(RolesAllowedDynamicFeature.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new App().run(args);
+	}
 }
